b4f9cf3f17a371d2f9c85f1628bbadf19790e40d
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-03-03 11:55:53 +0000

    create completed

--- src/main/java/com/qa/students/rest/StudentsController.java
+++ src/main/java/com/qa/students/rest/StudentsController.java
@@ -1,5 +1,35 @@
 package com.qa.students.rest;
 
-public class StudentsController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.students.domain.Students;
+import com.qa.students.service.StudentsService;
+
+@RestController
+public class StudentsController {
+	//crud
+
+	private StudentsService service;
+
+	public StudentsController(StudentsService service) {
+		super();
+		this.service = service;
+	}
+	
+	//create - post request
+	@PostMapping("/create")
+	public Students createMap(@RequestBody Students info) {
+		return this.service.create(info);
+	}
+	
+	//readAll - get request - list
+	//readById - get request - 1 record
+	
+	//update - put request - need id (path param or path variable) and a body
+	
+	//delete - delete request  - need id
+	
+	
 }
--- src/main/java/com/qa/students/service/StudentsService.java
+++ src/main/java/com/qa/students/service/StudentsService.java
@@ -1,5 +1,30 @@
 package com.qa.students.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.students.domain.Students;
+import com.qa.students.repo.StudentsRepo;
+
+@Service
 public class StudentsService {
 
+	private StudentsRepo repo;
+
+	public StudentsService(StudentsRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	
+	//create - logic
+	public Students create(Students newStudent) {
+		return this.repo.save(newStudent);
+	}
+	
+	//read - findAll, findById
+	
+	//update - getters & setters to update each field individually
+	
+	//delete - deleteById
+	
 }
